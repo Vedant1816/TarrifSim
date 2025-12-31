e4eb1340e598aa024c77fbd37140341e16837071
Vedant1816 <angravedant@gmail.com>
2025-12-31 18:21:47 +0530

    Modified backend for deployment

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",  
+    origin: ["http://localhost:5173", "https://tarrif-sim.vercel.app"],  
     credentials: true,
   })
 );
